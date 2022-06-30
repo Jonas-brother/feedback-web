e1d5263646d590a72912712192a9c577db902b87
VASLIN Jean <jean.vaslin@cnp.fr>
2022-06-30 14:20:03 +0200

    Update website

---
+++ assets/projects.3078104e.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as r,a as c}from"./index.cbb3cc20.js";const a={methods:{move(){console.log("move"),this.$router.push("/projects/WbA2RqSKaMXmY07Aiayc")}}},n={class:"text-center"};function p(m,e,i,l,_,t){return s(),r("div",n,[c("button",{onClick:e[0]||(e[0]=d=>t.move()),class:"btn btn-primary",type:"submit"},"Acces projet")])}var f=o(a,[["render",p]]);export{f as default};
